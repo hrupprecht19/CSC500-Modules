2e1455fd6202866858ff6a02a8da2c9d97deb565
hrupprecht19 <hrupprecht81@gmail.com>
2025-06-13 09:04:13 -0600

    Update CT1.py
    adding in the subtraction part, to the problem.

--- CT1.py
+++ CT1.py
@@ -8,8 +8,8 @@
 
 num1 = int(input('Please provide a number.'))
 num2 = int(input('Please provide another number.'))
-print( num1, '+', num2 )
-print(num1 - num2)
+print( num1, '+', num2, '=', num1 + num2 )
+print( num1, '-', num2, '=', num1 - num2 )
 
 
 
