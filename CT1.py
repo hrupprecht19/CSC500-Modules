582b86314f781807f1129f44ecbc18f846f6d676
hrupprecht19 <hrupprecht81@gmail.com>
2025-06-13 08:56:15 -0600

    Create CT1.py
    working on school work

---
+++ CT1.py
@@ -0,0 +1,27 @@
+#Creating Python Programs
+#Part 1:
+#Write a Python program to find the addition and subtraction of two numbers.
+
+#Ask the user to input two numbers (num1 and num2). Given those 
+#two numbers, add them together to find the output. Also, 
+#subtract the two numbers to find the output.
+
+num1 = int(input('Please provide a number.'))
+num2 = int(input('Please provide another number.'))
+print( num1, '+', num2 )
+print(num1 - num2)
+
+
+
+
+#Part 2:
+#Write a Python program to find the multiplication and division of two numbers.
+
+#Ask the user to input two numbers (num1 and num2). Given those two 
+#numbers, multiply them #together to find the output. Also, divide 
+#num1/num2 to find the output.
+
+#Compile and submit your pseudocode, source code, and screenshots of the 
+#application executing #the code from parts 1 and 2, the results and 
+#GIT repository in a single document (Word is #preferred).
+
