a881d471daf272ca7823d59768d3d01b86cee903
hrupprecht19 <hrupprecht81@gmail.com>
2025-07-05 15:18:40 -0600

    Update M4_Milestone.py
    added a loop in for the items

--- M4_Milestone.py
+++ M4_Milestone.py
@@ -8,14 +8,29 @@ class ItemToPurchase:
 # Example of print_item_cost() output: Bottled Water 10 @ $1 = $10
 #https://www.geeksforgeeks.org/python/class-instance-attributes-python/
     def print_item_cost(self):
-        print('{} {} @ $ {} = $ {}'.format(
+        print('{} on hand {} @ $ {:.2f} = $ {:.2f}'.format(
             self.item_name, 
             self.item_quantity, 
-            self.item_price , 
+            self.item_price, 
             self.item_total
             ))
+#a place to store the items added 
+shopping_cart = [ ]
 # get the input in the string, float, interger format        
-name = input("Enter i")
-price = float(input('Enter'))
-quantity = int(input("enter"))
+# loop for more than one item
+while True:
+    name = input("Enter the item name (or type 'done' to finish)")
+    if name == 'done':
+        break
+    price = float(input("Enter the price: "))
+    quantity = int(input("Enter the quantity: "))
+#FITIT added in a part for debuging the price and quantity
+    item = ItemToPurchase(name, price, quantity)
+    shopping_cart.append(item)
+
+print("\n Shopping Cart Summary:")
+for item in shopping_cart:
+    item.print_item_cost()
+
+
 
