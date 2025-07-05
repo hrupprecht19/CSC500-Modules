d03cee93df256c94d1205d467708710f01acd01f
hrupprecht19 <hrupprecht81@gmail.com>
2025-07-05 14:55:53 -0600

    Update M4_Milestone.py
    Trying to get the input in the right spot

--- M4_Milestone.py
+++ M4_Milestone.py
@@ -1,12 +1,21 @@
 #Build the ItemToPurchase class with the following specifications
 class ItemToPurchase:
     def __init__(self, item_name = 'none', item_price = 0, item_quantity = 0):
-        self.item_name
-        self.item_price
-        self.item_quantity
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
         self.item_total = (self.item_price * self.item_quantity)
-#* create print_item_cost
 # Example of print_item_cost() output: Bottled Water 10 @ $1 = $10
+#https://www.geeksforgeeks.org/python/class-instance-attributes-python/
     def print_item_cost(self):
-        #print(self.item_name, self.item_quantiy, @, self.item_price, =, self.item_total)
-        print(self.item_name(), self.item_quantiy(), "@ $",self.item_price (), " = $", self.item_total())
+        print('{} {} @ $ {} = $ {}'.format(
+            self.item_name, 
+            self.item_quantity, 
+            self.item_price , 
+            self.item_total
+            ))
+# get the input in the string, float, interger format        
+name = input("Enter i")
+price = float(input('Enter'))
+quantity = int(input("enter"))
+
